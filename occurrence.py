a897a55d02884572ae0d6747d0e9379aa9663e6f
Jagruthireddie <161810025+Jagruthireddie@users.noreply.github.com>
2025-09-22 09:34:33 +0530

    Delete occurrence.py

--- occurrence.py
+++
@@ -1,9 +0,0 @@
-def occur():
-    s=input("Enter string")
-    c=input("Enter the occurrence ")
-    l=[]
-    for i in range(len(s)):
-        if s[i]==c:
-            l.append(i)
-    print(l)
-occur()        
